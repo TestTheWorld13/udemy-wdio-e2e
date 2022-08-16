4187a44a497a2a619e95fdf3f8f9e90741bc6db9
“David <tennisgakki@gmail.com>
2022-08-16 17:35:28 +0900

    update Then.ts file for Inventory feature

--- test/features/step-definitions/Then.ts
+++ test/features/step-definitions/Then.ts
@@ -1,14 +1,34 @@
 import { Then } from "@wdio/cucumber-framework";
 import chai from "chai";
 
-Then(/^inventory page should list (.*)$/, async function (NumberOfProducts){
-    // IF there is not a valid number, then  throw an error
-    if(!NumberOfProducts) throw Error(`Invalid product count provided ${NumberOfProducts}`)
-    let eleArr = await $$('.inventory_item_name')
-    // 'parseInt' will convert the string into numbers
-    chai.expect(eleArr.length).to.equal(parseInt(NumberOfProducts)) // === compare its 'type' and 'value'
+Then(/^inventory page should list (.*)$/, async function (NumberOfProducts) {
+  // IF there is not a valid number, then  throw an error
+  if (!NumberOfProducts)
+    throw Error(`Invalid product count provided ${NumberOfProducts}`);
+  let eleArr = await $$(".inventory_item_name");
+  // 'parseInt' will convert the string into numbers
+  chai.expect(eleArr.length).to.equal(parseInt(NumberOfProducts)); // === compare its 'type' and 'value'
 });
 
-Then(/^validate all products have valid price$/, async function(){
-    
-})
+/**
+ * Steps
+ * 1. get price list
+ * 2. convert strings to numbers
+ * 3. Assert if any value is <= 0
+ */
+Then(/^validate all products have valid price$/, async function () {
+  /** 1. get price */
+  let eleArr = await $$(".inventory_item_price");
+  let priceStrArr = [];
+  for (let i = 0; i < eleArr.length; i++) {
+    let priceStr = await eleArr[i].getText();
+    priceStrArr.push(priceStr);
+  }
+  console.log(`>> price with $: ${priceStrArr}`);
+  /** 2. convert strings to numbers */
+  let priceNumArr = priceStrArr.map((ele) => +ele.replace("$", ""));
+  console.log(`>> price in number: ${priceNumArr}`);
+  /** 3. Assert if any value is <= 0 */
+  let invalidPriceArr = priceNumArr.filter((ele) => ele <= 0);
+  chai.expect(invalidPriceArr.length).to.equal(0);
+});
